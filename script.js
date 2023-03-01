120432543a3e53dfb0f411fd0bebbb1752468d79
Gabriel Pinheiro <112494218+Gabriel1Pinheiro@users.noreply.github.com>
2023-03-01 18:08:07 -0300

    Subindo projeto para o GitHubr

---
+++ script.js
@@ -0,0 +1,10 @@
+// botão do navbar
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+
